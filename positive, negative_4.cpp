372f5d2259826a9bd56cfab168ea4cac50ed42c2
bhumikapursharthi <bhumikapursharthi@gmail.com>
2026-07-05 14:37:52 +0530

    Add files via upload

---
+++ positive, negative_4.cpp
@@ -0,0 +1,15 @@
+
+#include<iostream>
+using namespace std;
+int main(){
+int a;
+cout<<"Enter the number:";
+cin>>a;
+if (a>0)
+    cout<<"The number is positive";
+else if (a<0)
+    cout<<"The number is negative";
+else
+    cout<<"The number is zero";
+
+}
