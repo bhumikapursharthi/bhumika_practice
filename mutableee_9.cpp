049b863287a07536b8c9f592bd2e55ef6abdbda2
bhumikapursharthi <bhumikapursharthi@gmail.com>
2026-07-05 14:41:37 +0530

    Add files via upload

---
+++ mutableee_9.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main (){
+;
+try(age>=18){
+    int age = 12
+cout<<"access granted";
+}
+else {
+
+    throw(age);
+
+}
+
+catch (int age){
+cout<<"Access Denied";
+}
+return 0;
+}
